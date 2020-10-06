4755f6369487706bc439e52d42e3e5533c945da9
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-06 18:49:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,19 @@
 require_relative "./user.rb"
 
 class Student < User
+  
+  attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = [] 
+  end 
+  
+  def learn(know) 
+    @knowledge << know 
+  end
+  
+  def knowledge 
+    @knowledge
+  end
 
 end
